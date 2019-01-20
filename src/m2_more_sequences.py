1728b6fff13b0d09f4530a71895c6d50c2c0e728
vandensp <vandensp@rose-hulman.edu>
2019-01-20 14:43:50 -0500

    Done

--- src/m2_more_sequences.py
+++ src/m2_more_sequences.py
@@ -18,8 +18,8 @@ def main():
     """ Calls the   TEST   functions in this module. """
     #run_test_shortest_string()
     #run_test_index_of_largest_number()
-    run_test_number_of_stutters()
-    run_test_is_palindrome()
+    #run_test_number_of_stutters()
+    #run_test_is_palindrome()
     run_test_count_same()
 
 
@@ -327,7 +327,7 @@ def is_palindrome(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -336,6 +336,12 @@ def is_palindrome(s):
     #   above are particularly good examples to work by hand.
     ####################################################################
     # ------------------------------------------------------------------
+
+    for k in range(len(s) // 2):
+        if s[k] != s[len(s) - 1 - k]:
+            return False
+    return True
+
 
 
 # ----------------------------------------------------------------------
@@ -394,11 +400,14 @@ def count_same(sequence1, sequence2):
       type: sequence2: tuple or list or string
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # Done: 6. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
-
-
+    count = 0
+    for k in range (len(sequence1)):
+        if sequence1[k] == sequence2[k]:
+            count = count + 1
+    return count
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # ----------------------------------------------------------------------
